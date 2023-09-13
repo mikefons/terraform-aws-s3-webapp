c2d589be98c22799a5b3e91035a3657350d14f94
mikefons <fonseca.michael@gmail.com>
2023-09-13 19:17:41 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -16,9 +16,17 @@ resource "aws_s3_bucket_website_configuration" "bucket" {
   }
 }
 
-resource "aws_s3_bucket_acl" "bucket" {
+resource "aws_s3_bucket_ownership_controls" "bucket" {
   bucket = aws_s3_bucket.bucket.id
+  rule {
+    object_ownership = "BucketOwnerPreferred"
+  }
+}
 
+resource "aws_s3_bucket_acl" "bucket" {
+  depends_on = [aws_s3_bucket_ownership_controls.bucket]
+
+  bucket = aws_s3_bucket.bucket.id
   acl = "private"
 }
 
