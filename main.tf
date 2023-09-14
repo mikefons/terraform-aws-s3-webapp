38356bc42201abb2f15a6d8f79ad41776d99d0d8
mikefons <fonseca.michael@gmail.com>
2023-09-13 21:48:23 -0400

    removed policy

--- main.tf
+++ main.tf
@@ -30,28 +30,6 @@ resource "aws_s3_bucket_acl" "bucket" {
   acl = "private"
 }
 
-resource "aws_s3_bucket_policy" "policy" {
-  bucket = aws_s3_bucket.bucket.id
-  policy = <<EOF
-{
-    "Version": "2012-10-17",
-    "Statement": [
-        {
-            "Sid": "PublicReadGetObject",
-            "Effect": "Allow",
-            "Principal": "*",
-            "Action": [
-                "s3:GetObject"
-            ],
-            "Resource": [
-                "arn:aws:s3:::${aws_s3_bucket.bucket.id}/*"
-            ]
-        }
-    ]
-}
-EOF
-}
-
 resource "aws_s3_object" "webapp" {
   acl          = "private"
   key          = "index.html"
